9976f80736ec77973dac48395580a1a57bb167e7
Ragy4a <122722327+Ragy4a@users.noreply.github.com>
2024-02-12 09:53:05 +0200

    Production Version #2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
 function calculateFactorialOfNumber (numb) {
-    let factorial = numb;
-    if (typeof numb !== 'number') {
+    if (typeof numb !== 'number' || numb < 0) {
         return 'Unknown information. Please, try again later!'
     };
     if (numb === 0) {
         return 1;
     };
-    return factorial * calculateFactorialOfNumber(factorial - 1);
+    return numb * calculateFactorialOfNumber(numb - 1);
 };
 
 console.log(calculateFactorialOfNumber(6))
 
-// Max numb === 8968, 8969 is RangeError: Maximum call stack size exceeded
+// Max numb === 8968, 8969 is RangeError: Maximum call stack size exceededs
